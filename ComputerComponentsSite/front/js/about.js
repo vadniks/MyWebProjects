f4ce2793daa64d43b8095b368302c73974adc890
vadniks <vadniks@fk.fk>
2022-03-02 00:37:03 +0300

    Dockerized

--- ComputerComponentsSite/front/js/about.js
+++
@@ -1,9 +0,0 @@
-import { loadLog } from "./global.js"
-
-window.onload = () => { setTimeout(() => {
-    loadLog()
-    document.getElementById('menuBtVr')
-        .addEventListener('click', () => window.location.replace('/'))
-    document.getElementById('menuBtLog')
-        .addEventListener('click', () => window.location.replace('/login'))
-}, 0) }
